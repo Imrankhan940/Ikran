dae236483fac441c129e02633fe8f6206058d507
Imrankhan940 <129730895+Imrankhan940@users.noreply.github.com>
2023-05-17 19:40:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var num = Math.floor(Math.random() * 2) + 1;
 
 $("#card").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Image/we3_card ${num}.jpg`, // Background (image path or hex color).
+  bg: `Image/we3_card${num}.jpg`, // Background (image path or hex color).
   fg: `Image/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
