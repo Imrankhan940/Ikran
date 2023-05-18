1f5599e65a75f63aa7f8c056320978d64e002e13
Imrankhan940 <129730895+Imrankhan940@users.noreply.github.com>
2023-05-18 09:29:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $("#card").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Image/we3_card$.jpg`, // Background (image path or hex color).
+  bg: `Image/istockphoto-175413535-612x612.jpg`, // Background (image path or hex color).
   fg: `Image/front.jpg`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
